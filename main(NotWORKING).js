9fe6f8acf47f70e24cc8ed342358645814912099
jtempels <julian.tempelsman@gmail.com>
2012-04-18 21:54:18 -0400

    updated

--- main(NotWORKING).js
+++ main(NotWORKING).js
@@ -33,13 +33,21 @@ function submenuItemClick (me) {
 
 function hideOthers(me) {
 	if (selectedContent !== me.id) {
-		$("."+selectedContent+"Content").hide(1500);
+		
+		if(selectedContent == "Home"){
+			$("."+selectedContent+"Content").hide(1000);
+		}
+			
+		else {
+		$("."+selectedContent+"Content").hide(1000);}
+		
 		if (subSelectedItem!= me) {
 			$(subSelectedItem).toggleClass("subItemSelected");
 			subSelectedItem= null;
 		}
 	}
-	selectedContent = me.id;		
+	selectedContent = me.id;	
+	console.log("Selected content is now: ", selectedContent);	
 }
 
 
@@ -60,12 +68,12 @@ $(document).ready(function() {
 			if (selected == null)
 				selected = this.id;
 			console.log(this);
-			console.log(this.id);	
+			console.log(this.id);		
 			switch(this.id) {
 				case "Home": 
 					menuHeadClick(this);
 					hideOthers(this);
-					$("#album").attr('src','Homepage2/album/Home.html');
+					$("#HomeAlbum").attr('src','Homepage2/album/Home.html');
 					$(".HomeContent").fadeIn(1000);
 					break;
 				case "Australia": 
@@ -77,8 +85,8 @@ $(document).ready(function() {
 				case "California": 
 					submenuHeadClick(this);
 					hideOthers(this);
-					$("#CaliforniaFrame").attr('src','Photos/MyAlbum/California.html');
-					$(".CaliforniaContent").fadeIn(1000);
+					$("#album").attr('src','Photos/MyAlbum/California.html');
+					$(".PhotoAlbums").fadeIn(1000);
 					break;
 				case "France": 
 					submenuHeadClick(this);
@@ -87,43 +95,43 @@ $(document).ready(function() {
 					//submenuHeadClick(this);
 					menuHeadClick(this);
 					hideOthers(this);
-					$("#JoshuaTreeFrame").attr('src','Photos/MyAlbum/JoshuaTree.html');
-					$(".JoshuaTreeContent").fadeIn(1000);
+					$("#album").attr('src','Photos/MyAlbum/JoshuaTree.html');
+					$(".PhotoAlbums").fadeIn(1000);
 					break;
 				case "London": 
 					//submenuHeadClick(this);
 					menuHeadClick(this);
 					hideOthers(this);
-					$("#LondonFrame").attr('src','Photos/MyAlbum/London.html');
-					$(".LondonContent").fadeIn(1000);
+					$("#album").attr('src','Photos/MyAlbum/London.html');
+					$(".PhotoAlbums").fadeIn(1000);
 					break;
 				case "MarthasVineyard": 
 					//submenuHeadClick(this);
 					menuHeadClick(this);
 					hideOthers(this);
-					$("#MarthasVineyardFrame").attr('src','Photos/MyAlbum/MarthasVineyard.html');
-					$(".MarthasVineyardContent").fadeIn(1000);
+					$("#album").attr('src','Photos/MyAlbum/MarthasVineyard.html');
+					$(".PhotoAlbums").fadeIn(1000);
 					break;
 				case "NewYorkCity": 
 					//submenuHeadClick(this);
 					menuHeadClick(this);
 					hideOthers(this);
-					$("#NewYorkCityFrame").attr('src','Photos/MyAlbum/NewYorkCity.html');
-					$(".NewYorkCityContent").fadeIn(1000);
+					$("#album").attr('src','Photos/MyAlbum/NewYorkCity.html');
+					$(".PhotoAlbums").fadeIn(1000);
 					break;
 				case "NewZealand": 
 					//submenuHeadClick(this);
 					menuHeadClick(this);
 					hideOthers(this);
-					$("#NewZealandFrame").attr('src','Photos/MyAlbum/NewZealand.html');
-					$(".NewZealandContent").fadeIn(1000);
+					$("#album").attr('src','Photos/MyAlbum/NewZealand.html');
+					$(".PhotoAlbums").fadeIn(1000);
 					break;
 				case "Weddings": 
 					//submenuHeadClick(this);
 					menuHeadClick(this);
 					hideOthers(this);
-					$("#WeddingsFrame").attr('src','Photos/MyAlbum/Wedding.html');
-					$(".WeddingsContent").fadeIn(1000);
+					$("#album").attr('src','Photos/MyAlbum/Wedding.html');
+					$(".PhotoAlbums").fadeIn(1000);
 					break;
 				case "Timelapses": 
 					submenuHeadClick(this);
@@ -136,11 +144,6 @@ $(document).ready(function() {
 					hideOthers(this);
 					$(".AboutUsContent").fadeIn(1000);
 					break;
-				case "ContactUs": 
-					submenuHeadClick(this);
-					hideOthers(this);
-					$(".ContactUsContent").fadeIn(1000);
-					break;
 			}});	
 			
 	$(".subMenuItem").click(function() {
@@ -151,67 +154,59 @@ $(document).ready(function() {
 			case "AustraliaUnderwater": 
 				submenuItemClick(this);
 				hideOthers(this);
-				$(".AustraliaUnderwaterContent").fadeIn(1000);
-				$("#AustraliaUnderwaterFrame").attr('src','Photos/MyAlbum/AustraliaUnderwater.html');
-				selectedContent = "AustraliaUnderwater";
+				$(".PhotoAlbums").fadeIn(1000);
+				$("#album").attr('src','Photos/MyAlbum/AustraliaUnderwater.html');
+				selectedContent = "HomeContent";
 				break;
 			case "AustraliaPlaces": 			
 				submenuItemClick(this);
 				hideOthers(this);
-				$(".AustraliaPlacesContent").fadeIn(1000);
-				$("#AustraliaPlacesFrame").attr('src',"Photos/MyAlbum/AustraliaPlaces.html");
-				selectedContent = "AustraliaPlaces";
+				$(".PhotoAlbums").fadeIn(1000);
+				$("#album").attr('src',"Photos/MyAlbum/AustraliaPlaces.html");
+				selectedContent = "HomeContent";
 				break;
 			case "AustraliaWildlife": 			
 			submenuItemClick(this);
 			hideOthers(this);
-			$(".AustraliaWildlifeContent").fadeIn(1000);
-			$("#AustraliaWildlifeFrame").attr('src','Photos/MyAlbum/AustraliaWildlife.html');
-			selectedContent = "AustraliaWildlife";
+			$(".PhotoAlbums").fadeIn(1000);
+			$("#album").attr('src','Photos/MyAlbum/AustraliaWildlife.html');
+			selectedContent = "HomeContent";
 			break;
 			case "BahamasUnderwater": 
 				submenuItemClick(this);
 				hideOthers(this);
-				$("#BahamasUnderwaterFrame").attr('src','Photos/MyAlbum/BahamasUnderwater.html');
-				$(".BahamasUnderwaterContent").fadeIn(1000);
-				selectedContent = "BahamasUnderwater";
+				$("#album").attr('src','Photos/MyAlbum/BahamasUnderwater.html');
+				$(".PhotoAlbums").fadeIn(1000);
+				selectedContent = "HomeContent";
 				break;								
 			case "BahamasPlaces": 			
 				submenuItemClick(this);
 				hideOthers(this);
-				$("#BahamasPlacesFrame").attr('src','Photos/MyAlbum/BahamasScenery.html');
-				$(".BahamasPlacesContent").fadeIn(1000);
-				selectedContent = "BahamasPlaces";
+				$("#album").attr('src','Photos/MyAlbum/BahamasScenery.html');
+				$(".PhotoAlbums").fadeIn(1000);
+				selectedContent = "HomeContent";
 				break;	
 			case "Etretat": 
 				submenuItemClick(this);
 				hideOthers(this);
-				$("#EtretatFrame").attr('src','Photos/MyAlbum/Etretat.html');
-				$(".EtretatContent").fadeIn(1000);
-				selectedContent = "Etretat";
+				$("#album").attr('src','Photos/MyAlbum/Etretat.html');
+				$(".PhotoAlbums").fadeIn(1000);
+				selectedContent = "HomeContent";
 				break;
 			case "Honfleur": 			
 				submenuItemClick(this);
 				hideOthers(this);
-				$("#HonfleurFrame").attr('src','Photos/MyAlbum/Honfleur.html');
-				$(".HonfleurContent").fadeIn(1000);
-				selectedContent = "Honfleur";
+				$("#album").attr('src','Photos/MyAlbum/Honfleur.html');
+				$(".PhotoAlbums").fadeIn(1000);
+				selectedContent = "HomeContent";
 				break;
 			case "Paris": 			
 			submenuItemClick(this);
 			hideOthers(this);
-			$("#ParisFrame").attr('src','Photos/MyAlbum/Paris.html');
-			$(".ParisContent").fadeIn(1000);
-			selectedContent = "Paris";
+			$("#album").attr('src','Photos/MyAlbum/Paris.html');
+			$(".PhotoAlbums").fadeIn(1000);
+			selectedContent = "HomeContent";
 			break;
-			
-			case "Contacts": 			
-				submenuItemClick(this);
-				hideOthers(this);
-				$(".ContactUsContent").fadeIn(1000);
-				selectedContent = "ContactUs";
-				break;
-			
 		}});
 
 });
